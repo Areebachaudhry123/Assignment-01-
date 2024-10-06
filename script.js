7e1deab0feee18ed2cf9158fafc685551ed5145b
usman_ahmad <usmanahmad417406@gmail.com>
2024-10-06 11:24:02 +0500

    Implemented smooth scrolling for navigation links with offset adjustments.

--- script.js
+++ script.js
@@ -39,3 +39,40 @@ $(document).ready(function() {
   }
 
 });
+
+
+$(document).ready(function() {
+
+  $(".header ul li a").click(function(e) {
+    e.preventDefault(); 
+  
+    var target = $(this).attr("href");
+  
+    if ($(target).hasClass("active-section")) {
+      return; 
+    }
+  
+    if (target === "#home") {
+      $("html, body").animate(
+        {
+          scrollTop: 0 
+        },
+        500
+      );
+    } else {
+      var offset = $(target).offset().top - 40; 
+  
+      $("html, body").animate(
+        {
+          scrollTop: offset
+        },
+        500
+      );
+    }
+  
+    $(".header ul li a").removeClass("active");
+    $(this).addClass("active");
+  });
+
+});
+
